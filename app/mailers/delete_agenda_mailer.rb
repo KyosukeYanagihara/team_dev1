de5194b0da94fd3637fac2802c7f113ae36f9c76
KyosukeYanagihara <rctttzg7@gmail.com>
2021-03-10 12:01:39 +0900

    アジェンダ削除後メール送信機能追加

---
+++ app/mailers/delete_agenda_mailer.rb
@@ -0,0 +1,6 @@
+class DeleteAgendaMailer < ApplicationMailer
+  def delete_agenda_mail(agenda)
+    @agenda = agenda
+    mail to: @agenda.team.members.pluck(:email), subject: 'アジェンダ削除通知メール'
+  end
+end
